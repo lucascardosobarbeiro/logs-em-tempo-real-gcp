6a336e16e75d56e8c4c41aa53e84af09fcac5b0f
Lucas Cardoso Barbeiro <lucas.barbeiro@educacao.sp.gov.br>
2025-05-09 16:51:40 -0300

    adicionando tratamento de variaveis ao main e zipando os process

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,16 +2,16 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-
-resource "google_pubsub_topic" "logs_topic" {
-  name = "logs-topic"
-}
-
-resource "google_storage_bucket" "function_bucket" {
-  name          = "${var.project_id}-function-code"
-  location      = var.region
-  force_destroy = true
-}
+# comentando para nao criar novamente
+# resource "google_pubsub_topic" "logs_topic" {
+#   name = "logs-topic"
+# }
+# comentando para nao criar novamente
+# resource "google_storage_bucket" "function_bucket" {
+#   name          = "${var.project_id}-function-code"
+#   location      = var.region
+#   force_destroy = true
+# }
 
 resource "google_cloudfunctions_function" "receive_logs" {
   name        = "receive-logs"
