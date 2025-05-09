6fb8a51143a1483b4274d50ee2e2faaa7fb9894d
Lucas Cardoso Barbeiro <lucas.barbeiro@educacao.sp.gov.br>
2025-05-09 16:40:52 -0300

    adicionando tratamento de variaveis ao main e zipando os process

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,35 +7,19 @@ resource "google_pubsub_topic" "logs_topic" {
   name = "logs-topic"
 }
 
-resource "google_storage_bucket" "function_bucket" {
-  name          = "${var.project_id}-function-code"
-  location      = var.region
-  force_destroy = true
-}
-
-resource "google_storage_bucket_object" "receive_logs_zip" {
-  name   = "receive_logs.zip"
-  bucket = google_storage_bucket.function_bucket.name
-  source = "../functions/receive_logs.zip"
-}
-
-resource "google_storage_bucket_object" "process_logs_zip" {
-  name   = "process_logs.zip"
-  bucket = google_storage_bucket.function_bucket.name
-  source = "../functions/process_logs.zip"
-}
+# resource "google_storage_bucket" "function_bucket" {
+#   name          = "${var.project_id}-function-code"
+#   location      = var.region
+#   force_destroy = true
+# }
 
 resource "google_cloudfunctions_function" "receive_logs" {
   name        = "receive-logs"
-  runtime     = "python310"
   entry_point = "receive_logs"
-
-  source_archive_bucket = google_storage_bucket.function_bucket.name
-  source_archive_object = google_storage_bucket_object.receive_logs_zip.name
-
+  runtime     = "python310"
+  source_directory = "../functions/receive_logs"
   trigger_http = true
   available_memory_mb = 128
-
   environment_variables = {
     LOG_TOPIC = google_pubsub_topic.logs_topic.name
   }
@@ -43,19 +27,16 @@ resource "google_cloudfunctions_function" "receive_logs" {
 
 resource "google_cloudfunctions_function" "process_logs" {
   name        = "process-logs"
-  runtime     = "python310"
   entry_point = "process_logs"
-
-  source_archive_bucket = google_storage_bucket.function_bucket.name
-  source_archive_object = google_storage_bucket_object.process_logs_zip.name
-
+  runtime     = "python310"
+  source_directory = "../functions/process_logs"
   event_trigger {
     event_type = "google.pubsub.topic.publish"
     resource   = google_pubsub_topic.logs_topic.id
   }
-
   environment_variables = {
-    SENDGRID_API_KEY = "NWGZvAXmTeGF6rUyyNbjqA"
-    ALERT_EMAIL_TO   = "lcb.barbeiro@gmail.com"
-  }
+  SENDGRID_API_KEY = "NWGZvAXmTeGF6rUyyNbjqA"
+  ALERT_EMAIL_TO   = "lcb.barbeiro@gmail.com"
+}
+
 }
