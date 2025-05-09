0526411321742e4a8d866c94d078460644ef47c6
Lucas Cardoso Barbeiro <lucas.barbeiro@educacao.sp.gov.br>
2025-05-09 16:46:15 -0300

    adicionando tratamento de variaveis ao main e zipando os process

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,9 +17,12 @@ resource "google_cloudfunctions_function" "receive_logs" {
   name        = "receive-logs"
   entry_point = "receive_logs"
   runtime     = "python310"
-  source_directory = "../functions/receive_logs"
   trigger_http = true
   available_memory_mb = 128
+
+  source_archive_bucket = google_storage_bucket.function_bucket.name
+  source_archive_object = "receive_logs.zip"
+
   environment_variables = {
     LOG_TOPIC = google_pubsub_topic.logs_topic.name
   }
@@ -29,14 +32,17 @@ resource "google_cloudfunctions_function" "process_logs" {
   name        = "process-logs"
   entry_point = "process_logs"
   runtime     = "python310"
-  source_directory = "../functions/process_logs"
+
+  source_archive_bucket = google_storage_bucket.function_bucket.name
+  source_archive_object = "process_logs.zip"
+
   event_trigger {
     event_type = "google.pubsub.topic.publish"
     resource   = google_pubsub_topic.logs_topic.id
   }
-  environment_variables = {
-  SENDGRID_API_KEY = "NWGZvAXmTeGF6rUyyNbjqA"
-  ALERT_EMAIL_TO   = "lcb.barbeiro@gmail.com"
-}
 
+  environment_variables = {
+    SENDGRID_API_KEY = "NWGZvAXmTeGF6rUyyNbjqA"
+    ALERT_EMAIL_TO   = "lcb.barbeiro@gmail.com"
+  }
 }
